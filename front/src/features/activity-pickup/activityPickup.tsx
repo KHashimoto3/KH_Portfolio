23ab3acda97b105a7c6f11dc8dff9a1b2453b7e9
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-13 16:27:06 +0900

    ActivityPickupに説明を追加した。

--- front/src/features/activity-pickup/activityPickup.tsx
+++ front/src/features/activity-pickup/activityPickup.tsx
@@ -2,8 +2,13 @@ import { SectionTitle } from "@/components/header/sectionTitle";
 
 export const ActivityPickup = () => {
   return (
-    <div id="activity-pickup" className="container m-8 flex">
+    <div id="activity-pickup" className="container m-8">
       <SectionTitle text="Activity Pickup" />
+      <div className="text-center">
+        <p>
+          私の取り組んでいる活動のうち、特にみていただきたい内容をピックアップしています。
+        </p>
+      </div>
     </div>
   );
 };
