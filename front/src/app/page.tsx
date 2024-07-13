17ae32ae8fa5ebe5bed1fbdb85ff1983461c8b2f
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-13 13:13:29 +0900

    基本の構成を書いた。

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <div id="header">ヘッダがここに入ります</div>
-      <div id="main">メインのコンテンツがここに入ります</div>
+      <div id="header" className="h-16">
+        ヘッダがここに入ります
+      </div>
+      <div id="home-img" className="h-100 bg-green-100">
+        トップページの画像がここに入ります
+      </div>
+      <div id="main" className="h-100">
+        メインのコンテンツがここに入ります
+      </div>
       <div id="footer">フッタがここに入ります</div>
     </div>
   );
