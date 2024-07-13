8d2abd102afbf70e157d7481a4fae42d91bc335c
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-13 13:58:41 +0900

    headerとhome-imgの装飾を行った。

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,11 +1,29 @@
+import Image from "next/image";
+import homeImage from "../../public/home-img.jpg";
+
 export default function Home() {
   return (
     <div>
-      <div id="header" className="h-16">
-        ヘッダがここに入ります
+      <div id="menu-header" className="menu-header">
+        <div className=" mx-3">
+          <h1>KHのポートフォリオ</h1>
+        </div>
       </div>
-      <div id="home-img" className="h-100 bg-green-100">
-        トップページの画像がここに入ります
+      <div
+        id="home-img"
+        className="relative max-h-100 overflow-hidden bg-green-100"
+      >
+        <Image
+          src={homeImage}
+          alt="home"
+          className="w-full h-auto object-cover"
+        />
+        <div
+          id="home-img-text"
+          className="absolute top-0 left-0 w-full h-full flex justify-center items-center"
+        >
+          <h1 className="text-5xl text-white">Welcome to our site</h1>
+        </div>
       </div>
       <div id="main" className="h-100">
         メインのコンテンツがここに入ります
