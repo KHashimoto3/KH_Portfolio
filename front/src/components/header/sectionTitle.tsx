96aaae8b93028ebd5ac64b8bb4cfc4c15daf502b
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-15 17:15:41 +0900

    セクションタイトルにアンダーラインをつけた

--- front/src/components/header/sectionTitle.tsx
+++ front/src/components/header/sectionTitle.tsx
@@ -6,7 +6,9 @@ export const SectionTitle = (props: Props) => {
   const { text } = props;
   return (
     <div className="m-8">
-      <h1 className="text-center text-4xl">{text}</h1>
+      <h1 className="text-center text-4xl underline decoration-sky-500 font-bold">
+        {text}
+      </h1>
     </div>
   );
 };
