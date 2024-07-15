337c55e2673a53af027e52945602716eba203e03
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-15 17:34:05 +0900

    about-meを記述した。

--- front/src/features/about-me/aboutMe.tsx
+++ front/src/features/about-me/aboutMe.tsx
@@ -1,11 +1,21 @@
 import { SectionTitle } from "@/components/header/sectionTitle";
+import Image from "next/image";
+import aboutMeImg from "../../../public/about-me-img.jpg";
 
 export const AboutMe = () => {
   return (
     <div id="about-me" className="w-full p-8 bg-white">
       <SectionTitle text="About Me" />
-      <div className="text-center">
-        <p>私について紹介します。</p>
+      <div className="flex flex-row mx-8">
+        <div className="basis-1/2 p-5">
+          <Image src={aboutMeImg} alt="about-me" />
+        </div>
+        <div className="basis-1/2 p-10 text-2xl">
+          <p>名前：KH</p>
+          <p>大学：私立理系大学　大学院</p>
+          <p>出身：茨城県</p>
+          <p>趣味：プログラミング、動画鑑賞、動画編集</p>
+        </div>
       </div>
     </div>
   );
