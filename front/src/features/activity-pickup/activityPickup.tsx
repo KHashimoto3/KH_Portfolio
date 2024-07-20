372613e3297534924c73838cfb267dd59c23cfc6
KHashimoto3 <hashi.koki385@gmail.com>
2024-07-20 09:42:37 +0900

    activityPickupで、カードを横並びで表示するようにした。

--- front/src/features/activity-pickup/activityPickup.tsx
+++ front/src/features/activity-pickup/activityPickup.tsx
@@ -5,11 +5,14 @@ export const ActivityPickup = () => {
   return (
     <div id="activity-pickup" className="w-full p-8 bg-white">
       <SectionTitle text="Activity Pickup" />
-      <div className="text-center">
+      <div className="text-center flex flex-col justify-center">
         <p>
           私の取り組んでいる活動のうち、特にみていただきたい内容をピックアップしています。
         </p>
-        <Card />
+        <div className="mt-8 w-full flex justify-center items-center gap-x-8">
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   );
